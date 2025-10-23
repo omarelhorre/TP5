3caed7e403d01d6e8346543b1921497b76d6067e
OMAR EL HORRE <elhorre.omar@etu.uae.ac.ma>
2025-10-23 18:06:49 +0100

    FInal Realase

---
+++ Note/oneMain.c
@@ -0,0 +1,23 @@
+#include "recherche.h"
+#include "sort.h"
+#include "usef.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void){
+    int n = demanderTaille();
+    int *t = (int *) malloc(sizeof(int)*n);
+    remplirTab(t,n);
+    printf("entrer jusqu'a quelle element vous voulez arreter votre tri");
+    int limTableauTrie;
+    scanf("%d",&limTableauTrie);
+    triInsert(t,limTableauTrie);
+    afficheTab(t,n);
+    // partie 2 de la qst 
+    int x = lireVal();
+    rechercheDicoTabTrie(t,limTableauTrie,x);
+    rechercheSeq(t+limTableauTrie,n-limTableauTrie,x);
+    free(t);
+
+}
+
---
+++ Note/usef.h
@@ -0,0 +1,6 @@
+
+int demanderTaille(void);
+void afficheTab (int *t , int n );
+void remplirTab(int *t,int n);
+int lireVal(void);
+void echanger(int *a,int *b);
